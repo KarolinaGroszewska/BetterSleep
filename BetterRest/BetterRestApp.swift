426d47f2df559ad1792d8ea7950d55ad6085cf73
KarolinaGroszewska <karolina.groszewska@gmail.com>
2023-01-13 15:44:27 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kari on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
